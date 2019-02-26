99af505a80842fb1d525ce25ea564964474f01a8
“SIeep” <Brandon_n_evans@live.com>
2019-02-25 21:29:35 -0600

    contacts with db WIP

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,7 @@
 class Api::ContactsController < ApplicationController
+  def contact_list
+    # get a contact from the db
+    @contact1 = Contact.first
+    render 'contact_display.json.jbuilder'
+  end
 end
